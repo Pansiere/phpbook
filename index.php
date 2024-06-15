21788b57690304ff8bebf2d3f821fbed824648dc
Joao Pedro V. Pansiere <ojp027@gmail.com>
2024-06-15 20:12:16 +0000

    update

--- index.php
+++ index.php
@@ -1,44 +1,52 @@
+<?php
+$tax_rate = 0.2;
+
+function calculate_running_total($price, $quantity)
+{
+    global $tax_rate;
+    static $running_total = 0;
+    $total = $price * $quantity;
+    $tax   = $total * $tax_rate;
+    $running_total = $running_total + $total + $tax;
+    return $running_total;
+}
+?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <title>PHP &amp; MySQL</title>
-    <link rel="stylesheet" type="text/css" href="css/styles.css" />
-  </head>
-  <body>
-    <h1 id="header">PHP &amp; MySQL</h1>
-    <h2>Code Samples</h2>
+
+<head>
+    <title>Global and Static Variables</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+
+<body>
+    <h1>The Candy Store</h1>
     <table>
-      <tr><td class="section">Section A: Basic Programming Instructions</td></tr>
-      <tr><td><a href="section_a/intro/">Introduction</a></td></tr>
-      <tr><td><a href="section_a/c01/">Chapter 1: Variables, Expressions and Operators</a></td></tr>
-      <tr><td><a href="section_a/c02/">Chapter 2: Control Structures</a></td></tr>
-      <tr><td><a href="section_a/c03/">Chapter 3: Functions</a></td></tr>
-      <tr><td><a href="section_a/c04/">Chapter 4: Objects</a></td></tr>
-
-      <tr><td class="section">Section B: Dynamic Web Pages</td></tr>
-      <tr><td><a href="section_b/intro/">Introduction</a></td></tr>
-      <tr><td><a href="section_b/c05/">Chapter 5: Built-in Functions</a></td></tr>
-      <tr><td><a href="section_b/c06/">Chapter 6: Getting Data from Browsers</a></td></tr>
-      <tr><td><a href="section_b/c07/">Chapter 7: Images and Files</a></td></tr>
-      <tr><td><a href="section_b/c08/">Chapter 8: Dates and Times</a></td></tr>
-      <tr><td><a href="section_b/c09/">Chapter 9: Cookies and Sessions</a></td></tr>
-      <tr><td><a href="section_b/c10/">Chapter 10: Error Handling</a></td></tr>
-
-      <tr><td class="section">Section C: Database Driven Websites</td></tr>
-      <tr><td>
-          <p><b>NOTE</b>: To run the rest of the code you must:<br><br>
-          <b>A:</b> Set up a MySQL database on your local machine<br>
-          <b>B:</b> Update the database connection code in each chapter<br></p>
-      </td></tr>
-      <tr><td><a href="section_c/c11/">Chapter 11: SQL</a></td></tr>
-      <tr><td><a href="section_c/c12/">Chapter 12: Getting Data from a Database</a></td></tr>
-      <tr><td><a href="section_c/c13/">Chapter 13: Updating Data in the Database</a></td></tr>
-
-      <tr><td class="section">Section D: Extending the Application</td></tr>
-      <tr><td><a href="section_d/c14/public/index.php">Chapter 14: Refactoring and Dependency Injection</a></td></tr>
-      <tr><td><a href="section_d/c15/public/index.php">Chapter 15: Namespaces and Libraries</a></td></tr>
-      <tr><td><a href="section_d/c16/public/index.php">Chapter 16: Membership</a></td></tr>
-      <tr><td><a href="section_d/c17/public/">Chapter 17: Adding New Features</a></td></tr>
+        <tr>
+            <th>Item</th>
+            <th>Price</th>
+            <th>Qty</th>
+            <th>Running total</th>
+        </tr>
+        <tr>
+            <td>Mints:</td>
+            <td>$2</td>
+            <td>5</td>
+            <td>$<?= calculate_running_total(2, 5) ?></td>
+        </tr>
+        <tr>
+            <td>Toffee:</td>
+            <td>$3</td>
+            <td>5</td>
+            <td>$<?= calculate_running_total(3, 5) ?></td>
+        </tr>
+        <tr>
+            <td>Fudge:</td>
+            <td>$5</td>
+            <td>4</td>
+            <td>$<?= calculate_running_total(5, 4) ?></td>
+        </tr>
     </table>
-  </body>
+</body>
+
 </html>
